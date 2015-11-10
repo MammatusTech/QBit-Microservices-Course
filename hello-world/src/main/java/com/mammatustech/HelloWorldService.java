6ae58635277b54472b08f7b28583ea0346bbb12a
fadihub <fadimaalouli@gmail.com>
2015-11-09 22:04:26 -0800

    With hello-world example

--- hello-world/src/main/java/com/mammatustech/HelloWorldService.java
+++ hello-world/src/main/java/com/mammatustech/HelloWorldService.java
@@ -4,55 +4,7 @@ package com.mammatustech;
 import io.advantageous.qbit.admin.ManagedServiceBuilder;
 import io.advantageous.qbit.annotation.RequestMapping;
 
-/**
- * Default port for admin is 7777.
- * Default port for main endpoint is 8080.
- *
- * <pre>
- * <code>
- *
- *     Access the service:
- *
- *    $ curl http://localhost:8080/root/hello/hello
- *
- *
- *     To see swagger file for this service:
- *
- *    $ curl http://localhost:7777/__admin/meta/
- *
- *     To see health for this service:
- *
- *    $ curl http://localhost:8080/__health
- *     Returns "ok" if all registered health systems are healthy.
- *
- *     OR if same port endpoint health is disabled then:
- *
- *    $ curl http://localhost:7777/__admin/ok
- *     Returns "true" if all registered health systems are healthy.
- *
- *
- *     A node is a service, service bundle, queue, or server endpoint that is being monitored.
- *
- *     List all service nodes or endpoints
- *
- *    $ curl http://localhost:7777/__admin/all-nodes/
- *
- *
- *      List healthy nodes by name:
- *
- *    $ curl http://localhost:7777/__admin/healthy-nodes/
- *
- *      List complete node information:
- *
- *    $ curl http://localhost:7777/__admin/load-nodes/
- *
- *
- *      Show service stats and metrics
- *
- *    $ curl http://localhost:8080/__stats/instance
- * </code>
- * </pre>
- */
+
 @RequestMapping("/hello")
 public class HelloWorldService {
 
